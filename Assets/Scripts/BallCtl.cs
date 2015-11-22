71d516bcb57890d1e61277ee1f3b23fc73a9037f
ChrisLHall <mrchrislhall@gmail.com>
2015-11-21 18:57:30 -0800

    Multiplayer ball, untested.

--- Assets/Scripts/BallCtl.cs
+++ Assets/Scripts/BallCtl.cs
@@ -1,17 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class BallCtl : MonoBehaviour {
+public class BallCtl : NetworkBehaviour {
+	struct RBInfo {
+		public Vector3 position;
+		public Vector3 vel;
+	}
 	Rigidbody rb;
+
+	[SyncVar]
+	RBInfo physState;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		InitState();
+	}
+
+	[Server]
+	void InitState () {
+		rb.isKinematic = false;
 		rb.velocity = new Vector3(Random.value - 0.5f, Random.value - 0.5f, Random.value - 0.5f) * 10f;
+		physState.position = rb.position;
+		physState.vel = rb.velocity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		TestRandMotion();
+		SyncState();
+	}
+
 	
+	void SyncState () {
+		if (isServer) {
+			physState.position = rb.position;
+			physState.vel = rb.velocity;
+		} else {
+			rb.position = physState.position;
+			rb.velocity = physState.vel;
+		}
+	}
+
+	[Server]
+	void TestRandMotion () {
+		if (Random.value < 0.005f) {
+			rb.velocity = new Vector3(Random.value, Random.value, Random.value) - Vector3.one * 0.5f;
+		}
+		rb.velocity = rb.velocity.normalized * 5f;
 	}
 }
