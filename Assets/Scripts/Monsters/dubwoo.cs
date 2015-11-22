02751e4093a9f7553400dab8b0a96ba063a93ba0
iangoegebuer <ian@iangoegebuer.com>
2015-11-22 09:41:18 -0800

    Added ghost guy for speed

---
+++ Assets/Scripts/Monsters/dubwoo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class dubwoo : Squat {
+	public GameObject ballPrefab;
+	public float ballExpires;
+	public float waitToPoop;
+	protected override void Start () {
+		base.Start();
+		if (isServer) {
+			//StartCoroutine(PoopBall());
+		}
+	}
+	
+	protected override void Update () {
+		base.Update();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (isServer) {
+			if(other.GetComponent<BallCtl>() != null) {
+				Debug.Log ("Collide with ball");
+				other.GetComponent<BallCtl>().Accelerate();
+			}
+		}
+	}
+}
